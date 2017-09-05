1e70e72510ba1a5e4c11de0e377b98bebc523f41
Blachy146 <blachypk@gmail.com>
2017-09-05 22:27:30 +0200

    Added movie critics

---
+++ critics.py
@@ -0,0 +1,54 @@
+
+
+critics = {
+    'Lisa Rose': {
+        'Kobieta w blekitnej wodzie': 2.5,
+        'Weze w samolocie': 3.5,
+        'Cale szczescie': 3.0,
+        'Superman: Powrot': 3.5,
+        'Ja, ty i on': 2.5,
+        'Nocny sluchacz': 3.0
+    },
+    'Gene Saymour': {
+        'Kobieta w blekitnej wodzie': 3.0,
+        'Weze w samolocie': 3.5,
+        'Cale szczescie': 1.5,
+        'Superman: Powrot': 5.0,
+        'Ja, ty i on': 3.5,
+        'Nocny sluchacz': 3.0
+    },
+    'Michael Phillips': {
+        'Kobieta w blekitnej wodzie': 2.5,
+        'Weze w samolocie': 3.0,
+        'Superman: Powrot': 3.5,
+        'Ja, ty i on': 3.5,
+        'Nocny sluchacz': 3.0
+    },
+    'Claudia Puig': {
+        'Weze w samolocie': 3.5,
+        'Cale szczescie': 3.0,
+        'Superman: Powrot': 4.0,
+        'Ja, ty i on': 2.5,
+        'Nocny sluchacz': 4.5
+    },
+    'Mick LaSalle': {
+        'Kobieta w blekitnej wodzie': 3.0,
+        'Weze w samolocie': 4.0,
+        'Cale szczescie': 2.0,
+        'Superman: Powrot': 3.0,
+        'Ja, ty i on': 2.0,
+        'Nocny sluchacz': 3.0
+    },
+    'Jack Matthews': {
+        'Kobieta w blekitnej wodzie': 3.0,
+        'Weze w samolocie': 4.0,
+        'Superman: Powrot': 5.0,
+        'Ja, ty i on': 3.5,
+        'Nocny sluchacz': 3.0
+    },
+    'Toby': {
+        'Weze w samolocie': 4.5,
+        'Superman: Powrot': 4.0,
+        'Ja, ty i on': 1.0,
+    },
+}
